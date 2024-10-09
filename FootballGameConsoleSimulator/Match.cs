5632c257f22ac2bd76a89ec2ecefe4421b2aaa0d
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-09 17:40:24 +0400

    ctrate Match class and add attributes

---
+++ FootballGameConsoleSimulator/Match.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGameConsoleSimulator
+{
+    public class Match
+    {
+        // private attributes
+        private Team team1;
+        private Team team2;
+        private int currentTurn;
+        private bool extraRound;
+        private List <MidpointRounding> roundDetails;
+
+    }
+}
