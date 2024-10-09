81f56cbb9dea271ea9225db029ec27e879d52ca7
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-09 16:01:21 +0400

    method to allow the user to choose a formation

--- FootballGameConsoleSimulator/Team.cs
+++ FootballGameConsoleSimulator/Team.cs
@@ -21,7 +21,29 @@ namespace FootballGameConsoleSimulator
             this.score = 0;
         }
         /////////Methods//////////
-       
+        //method to allow the user to choose a formation
+       public void chooseFormation() 
+        {
+            Console.WriteLine("Choose a formation: [1] 4-4-2, [2] 4-3-3 [3] 3-5-2");
+            string choice=Console.ReadLine();
+            switch (choice) 
+            {
+                case "1":
+                    formation = "4-4-2";
+                    break;
+                case "2":
+                    formation = "4-3-3";
+                    break;
+                case "3":
+                    formation = "3-5-2";
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice, defaulting to 4-4-2.");
+                    formation = "4-4-2";
+                    break;
+            }
+            Console.WriteLine($"{teamName} selected formation: {formation}");
+        }
 
     }
 }
