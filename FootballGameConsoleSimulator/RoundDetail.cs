719793f2daac413511cc509365096c08bab5b2ef
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-09 20:00:42 +0400

    constructor for RoundDetail

--- FootballGameConsoleSimulator/RoundDetail.cs
+++ FootballGameConsoleSimulator/RoundDetail.cs
@@ -13,5 +13,22 @@ namespace FootballGameConsoleSimulator
         private string defendingTeam;
         private List<Player> attackingplayers;
         private List<Player> defendingplayers;
+        private int attackPower;
+        private int defendingPower;
+        private string outcome;
+
+
+        // constructor
+        public RoundDetail(string attackingTeam, string defendingTeam, List<Player> attackingplayers, List<Player> defendingplayers, int attackPower, int defendingPower, string outcome)
+        {
+           this.attackingTeam = attackingTeam;
+            this.defendingTeam = defendingTeam;
+            this.attackingplayers = attackingplayers;
+            this.defendingplayers = defendingplayers;
+            this.attackPower=attackPower;
+            this.defendingPower=defendingPower;
+            this.outcome = outcome;
+        }
+
     }
 }
