aa0b756376585a5bb0957fe6e3da61a433cc09a1
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-09 17:48:56 +0400

    constructor to initialize the match between two teams

--- FootballGameConsoleSimulator/Match.cs
+++ FootballGameConsoleSimulator/Match.cs
@@ -12,8 +12,22 @@ namespace FootballGameConsoleSimulator
         private Team team1;
         private Team team2;
         private int currentTurn;
+        private int currentHalf;
         private bool extraRound;
-        private List <MidpointRounding> roundDetails;
+        private List <RoundDetasil> roundDetails;
+
+        //constructor to initialize the match between two teams
+        public Match(Team team1, Team team2)
+        {
+
+            this.team1 = team1;
+            this.team2 = team2;
+            this.currentTurn = 0;
+            this.extraRound = false;
+            this.currentHalf = 1;
+            this.roundDetails=new List<RoundDetasil> ();
+        }
+
 
     }
 }
