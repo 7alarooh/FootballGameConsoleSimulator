f36581e64719ee610b1632d51fe503127da366cf
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-09 14:46:35 +0400

    enum Position in Player class

---
+++ FootballGameConsoleSimulator/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGameConsoleSimulator
+{
+    public enum Position 
+    {
+        Forward,
+        Midfielder,
+        Defender,
+        Goalkeeper
+    }
+    public class Player
+    {
+    }
+}
