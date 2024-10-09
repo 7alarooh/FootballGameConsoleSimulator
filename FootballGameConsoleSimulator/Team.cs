0a3dee77cd99ca121147391be55af2c7af1102ab
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-09 15:30:50 +0400

    create Team class and add private attributes

---
+++ FootballGameConsoleSimulator/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGameConsoleSimulator
+{
+    public class Team
+    {
+        //private attributes
+        private string teamName;
+        private List<Player> players;
+        private string formation;
+        private int score;
+    }
+}
