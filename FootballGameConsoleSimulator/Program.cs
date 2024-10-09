5559cec5f7f3e308305540e3b8689a2cfd06dae7
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-09 20:31:44 +0400

    create SoccerGameSimulator class to hadles the main menu anfd flow of game

--- FootballGameConsoleSimulator/Program.cs
+++ FootballGameConsoleSimulator/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace FootballGameConsoleSimulator
+﻿using System;
+using System.Collections.Generic;
+namespace FootballGameConsoleSimulator
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SoccerGameSimulator simulator = new SoccerGameSimulator();
+
+        }
+    }
+    public class SoccerGameSimulator
+    {
+        private Match match;
+        // Main method to run the game
+        public void Run()
+        {
+            bool running = true;
+            while (running)
+            {
+                Console.Clear();
+                DisplayMainMenu();
+                Console.Write("\nPlease select an option: ");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        StartNewMatch();
+                        break;
+                    case "2":
+                        DisplayMatchDetails();
+                        break;
+                    case "3":
+                        Console.WriteLine("\nThank you for playing! Exiting...");
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("\nInvalid option. Please try again.");
+                        break;
+                }
+                // Pause before returning to the menu
+                if (running)
+                {
+                    Console.WriteLine("\nPress any key to return to the main menu...");
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
